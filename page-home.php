ea2586f9bf2ae19faf50284cd29b72f6f64adaf6
vbaghdas <vbaghdas@gmail.com>
2018-02-02 01:08:12 -0800

    completed home page

--- page-home.php
+++ page-home.php
@@ -11,6 +11,40 @@ $course_url = get_post_meta( 10, 'course_url', true );
 $button_text = get_post_meta( 10, 'button_text', true );
 $optin_text = get_post_meta( 10, 'optin_text', true );
 
+// Advanced Custom Fields
+$income_feature_image = get_field('income_feature_image');
+$income_section_title = get_field('income_section_title');
+$income_section_desc = get_field('income_section_description');
+$reason_1_title = get_field('reason_1_title');
+$reason_1_desc = get_field('reason_1_description');
+$reason_2_title = get_field('reason_2_title');
+$reason_2_desc = get_field('reason_2_description');
+
+$who_feature_image = get_field('who_feature_image');
+$who_section_title = get_field('who_section_title');
+$who_section_body = get_field('who_section_body');
+
+$features_section_image = get_field('features_section_image');
+$features_section_title = get_field('features_section_title');
+$features_section_body = get_field('features_section_body');
+
+$project_features_title = get_field('project_feature_title');
+$project_features_body = get_field('project_feature_body');
+
+$course_introduction_title = get_field('course_introduction_title');
+$course_introduction_video = get_field('course_introduction_video');
+
+$instructor_section_title = get_field('instructor_section_title');
+$instructor_name = get_field('instructor_name');
+$bio_excerpt = get_field('bio_excerpt');
+$full_bio = get_field('full_bio');
+$twitter_username = get_field('twitter_username');
+$facebook_username = get_field('facebook_username');
+$google_plus_username = get_field('google_plus_username');
+$num_students = get_field('num_students');
+$num_reviews = get_field('num_reviews');
+$num_courses = get_field('num_courses');
+
 get_header(); ?>
 
 <!-- HERO
@@ -79,20 +113,26 @@ get_header(); ?>
 		<div class="container">
 			
 			<div class="section-header">
-				<img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/icon-boost.png" alt="Chart">
-				<h2>How You Can Boost Your Income</h2>
+
+                <!-- if user uploaded an image -->
+                <?php if( !empty($income_feature_image) ) : ?>
+                    <img src="<?php echo $income_feature_image['url']; ?>" alt="<?php echo $income_feature_image['alt']; ?>">
+                <?php endif; ?>
+                    
+				
+				<h2><?php echo $income_section_title; ?></h2>
 			</div><!-- section-header -->
 			
-			<p class="lead">Whether you&rsquo;re a freelance designer, entrepreneur, employee for a company, code hobbyist, or looking for a new career &mdash; this course gives you an immensely valuable skill that will enable you to either:</p>
+			<p class="lead"><?php echo $income_section_desc; ?></p>
 			<div class="row">
 				<div class="col-sm-6">
-					<h3>Make money on the side</h3>
-					<p>So you can save up for that Hawaiian vacation you&rsquo;ve been wanting, help pay off your debt, your car, your mortgage, or simply just to have bonus cash laying around.</p>
+					<h3><?php echo $reason_1_title; ?></h3>
+					<p><?php echo $reason_1_desc; ?></p>
 				</div><!-- end col -->
 				
 				<div class="col-sm-6">
-					<h3>Create a full-time income</h3>
-					<p>WordPress developers have options. Many developers make a generous living off of creating custom WordPress themes and selling them on websites like ThemeForest. Freelance designers and developers can also take on WordPress projects and make an extra $1,000 - $5,000+ per month.</p>
+					<h3><?php echo $reason_2_title; ?></h3>
+					<p><?php echo $reason_2_desc; ?></p>
 				</div><!-- end col -->
 			</div><!-- row -->
 		
@@ -106,32 +146,21 @@ get_header(); ?>
 		<div class="container">
 			
 			<div class="section-header">
-				<img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/icon-pad.png" alt="Pad and pencil">
-				<h2>Who Should Take This Course?</h2>
+                <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/icon-pad.png" alt="Pad and pencil">
+                
+                <!-- if user uploaded an image -->
+                <?php if( !empty($who_feature_image) ) : ?>
+                    <img src="<?php echo $who_feature_image['url']; ?>" alt="<?php echo $who_feature_image['alt']; ?>">
+                <?php endif; ?>
+
+				<h2><?php echo $who_section_title; ?></h2>
 			</div><!-- section-header -->
 			
 			<div class="row">
 				<div class="col-sm-8 col-sm-offset-2">
-				
-					<h3>Graphic &amp; Web Designers</h3>
-					<p>Graphic designers are extremely talented, but ask them to code their designs and they'll freeze up! This leaves them with no other choice but to hire a web developer. Any professional graphic designers knows web developers can be expensive.</p>
-					<p>If you&rsquo;re a designer, learning to code your own WordPress websites can change your business entirely! Now, not only are you a great designer, but you're a skillful developer, too! This puts you in a position to <strong>make an extra $1,000 - $5,000 per project.</strong></p>
 
-					<h3>Entrepreneurs</h3>
-					<p>Entrepreneurs have big dreams, and in many cases, shoestring budgets. In order to survive in the cut-throat world of the Startup company, it&rsquo;s a necessity to have a world-class website.  However, world-class websites come with a large price tag.</p>
-					<p>If you can learn how to build a high-quality startup website by yourself, then you&rsquo;ve just saved yourself a lot of cash, <strong>tens of thousands of dollars in many cases.</strong></p>
-
-					<h3>Employees</h3>
-					<p>Any company knows the education &amp; training of their employees is key to a thriving team.</p>
-					<p>Depending on the type of company you work for, if you understand how to code, and can develop CMS driven websites, that gives you <strong>negotiating power for a better position, or a higher salary.</strong></p>
-
-					<h3>Code Hobbyists</h3>
-					<p>It&rsquo;s fun to learn challenging new skills. Code hobbyists can add dynamic websites to their arsenal of tools to play with &mdash; you can even <strong>sell WordPress themes and plugins for cash!</strong> The possibilities are truly endless.</p>
-
-					<h3>People Looking for a New Career</h3>
-					<p>Are you out of work? Looking for a more rewarding job? Desire a career that can allow you to work almost anywhere in the world? Becoming a Web Developer might be the answer for you.</p>
-					<p><strong>Web developers are paid well, anywhere from $33,000 to more than $105,000 per year.</strong> They get to work at amazing companies that are changing the world, or they enjoy the ability to start their own companies, become location-independent and work from home, from coffee shops, in an airplane, on the beach, or wherever they want!</p>
-					
+                    <?php echo $who_section_body?>
+                    
 				</div><!-- end col -->
 			</div><!-- row -->
 
@@ -145,42 +174,40 @@ get_header(); ?>
 		<div class="container">
 		
 			<div class="section-header">
-				<img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/icon-rocket.png" alt="Rocket">
-				<h2>Course Features</h2>
+				
+				<!-- if user uploaded an image -->
+				<?php if( !empty($features_section_image) ) : ?>
+                    <img src="<?php echo $features_section_image['url']; ?>" alt="<?php echo $features_section_image['alt']; ?>">
+                <?php endif; ?>
+
+				<h2><?php echo $feature_section_title; ?></h2>
+
+				<!-- If user added body text -->
+				<?php if(!empty($features_section_body) ) : ?>
+
+				<p class="lead"><?php echo $feature_section_body ?></p>
+
+				<?php endif; ?>
+
 			</div><!-- section-header -->
 			
 			<div class="row">
-				
+
+				<?php $loop = new WP_Query( array( 
+					'post_type' => 'course_feature',
+					'orderby' => 'post_id',
+					'order' => 'ASC' 
+				) ) ?>
+
+				<?php while( $loop->have_posts() ) : $loop->the_post(); ?>
+
 				<div class="col-sm-2">
-					<i class="ci ci-computer"></i>
-					<h4>Lifetime access to 80+ lectures</h4>
+					<i class="<?php the_field('course_feature_icon'); ?>"></i>
+					<h4><?php the_title(); ?></h4>
 				</div><!-- end col -->
-				
-				<div class="col-sm-2">
-					<i class="ci ci-watch"></i>
-					<h4>10+ hours of HD video content</h4>
-				</div><!-- end col -->
-				
-				<div class="col-sm-2">
-					<i class="ci ci-calendar"></i>
-					<h4>30-day money back guarantee</h4>
-				</div><!-- end col -->
-				
-				<div class="col-sm-2">
-					<i class="ci ci-community"></i>
-					<h4>Access to a community of like-minded students</h4>
-				</div><!-- end col -->
-				
-				<div class="col-sm-2">
-					<i class="ci ci-instructor"></i>
-					<h4>Direct access to the instructor</h4>
-				</div><!-- end col -->
-				
-				<div class="col-sm-2">
-					<i class="ci ci-device"></i>
-					<h4>Accessible content on your mobile devices</h4>
-				</div><!-- end col -->
-				
+
+				<?php endwhile; ?>
+
 			</div><!-- row -->
 		</div><!-- container -->
 	</section><!-- course-features -->
@@ -191,25 +218,32 @@ get_header(); ?>
 	<section id="project-features">
 		<div class="container">
 		
-			<h2>Final Project Features</h2>
-			<p class="lead">Throughout this entire course, you work towards building an incredibly beautiful website. Want to see the website <strong>you</strong> are going to build? <em>You're looking at it!</em> The website you're using right now is the website you will have built entirely by yourself, by the end of this course.</p>
+			<h2><?php echo $project_feature_title; ?></h2>
+			<p class="lead"><?php echo $project_feature_body; ?></p>
 			
 			<div class="row">
+
+			<?php $loop = new WP_Query( array( 
+					'post_type' => 'project_feature',
+					'orderby' => 'post_id',
+					'order' => 'ASC' 
+				) ) ?>
+
+				<?php while( $loop->have_posts() ) : $loop->the_post(); ?>
+
 				<div class="col-sm-4">
-					<img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/icon-design.png" alt="Design">
-					<h3>Sexy &amp; Modern Design</h3>
-					<p>You get to work with a modern, professional quality design &amp; layout.</p>
+
+					<?php 
+						if ( has_post_thumbnail() ) {
+							the_post_thumbnail();
+						}
+					?>
+					
+					<h3><?php the_title(); ?></h3>
+					<p><?php the_content(); ?></p>
 				</div><!-- col -->
-				<div class="col-sm-4">
-					<img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/icon-code.png" alt="Code">
-					<h3>Quality HTML5 &amp; CSS3</h3>
-					<p>You'll learn how hand-craft a stunning website with valid, semantic and beautiful HTML5 &amp; CSS3.</p>
-				</div><!-- col -->
-				<div class="col-sm-4">
-					<img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/icon-cms.png" alt="CMS">
-					<h3>Easy-to-use CMS</h3>
-					<p>Allow your clients to easily update their websites by converting your static websites to dynamic websites, using WordPress.</p>
-				</div><!-- col -->
+
+				<?php endwhile; ?>
 				
 			</div><!-- row -->
 			
@@ -223,8 +257,8 @@ get_header(); ?>
 		<div class="container">
 			<div class="row">
 				<div class="col-sm-8 col-sm-offset-2">
-					<h2>Watch the Course Introduction</h2>
-					<iframe width="100%" height="415" src="//www.youtube.com/embed/q-mJJsnOHew" frameborder="0" allowfullscreen></iframe>
+					<h2><?php echo $course_introduction_title; ?></h2>
+					<iframe width="100%" height="415" src="//<?php echo $course_introduction_video ?>" frameborder="0" allowfullscreen></iframe>
 				</div><!-- end col -->
 			</div><!-- row -->			
 		</div><!-- container -->
@@ -239,23 +273,27 @@ get_header(); ?>
 				<div class="col-sm-8 col-md-6">
 					<div class="row">
 						<div class="col-lg-8">
-							<h2>Your Instructor <small>Brad Hussey</small></h2>
+							<h2><?php echo $instructor_section_title ?> <small><?php echo $instructor_name ?></small></h2>
 						</div><!-- end col -->
 						<div class="col-lg-4">
-							<a href="https://twitter.com/bradhussey" class="badge social twitter" target="_blank"><i class="fa fa-twitter"></i></a>
-							<a href="https://facebook.com/bradhussey" class="badge social facebook" target="_blank"><i class="fa fa-facebook"></i></a>
-							<a href="https://plus.google.com/+BradHussey" class="badge social gplus" target="_blank"><i class="fa fa-google-plus"></i></a>
+							<?php if ( !empty($twitter_username) ) : ?>
+							<a href="https://twitter.com/<?php echo $twitter_username ?>" class="badge social twitter" target="_blank"><i class="fa fa-twitter"></i></a>
+							<?php endif; ?>
+
+							<?php if ( !empty($facebook_username) ) : ?>
+							<a href="https://facebook.com/<?php echo $facebook_username ?>" class="badge social facebook" target="_blank"><i class="fa fa-facebook"></i></a>
+							<?php endif; ?>
+							
+							<?php if ( !empty($google_plus_username) ) : ?>
+							<a href="https://plus.google.com/<?php echo $google_plus_username ?>" class="badge social gplus" target="_blank"><i class="fa fa-google-plus"></i></a>
+							<?php endif; ?>
 						</div><!-- end col -->
 					
 					</div><!-- row -->
 					
-					<p class="lead">A highly skilled professional, Brad Hussey is a passionate and experienced web designer, developer, blogger and digital entrepreneur.<p>
+					<p class="lead"><?php echo $bio_excerpt ?><p>
 					
-					<p>Hailing from North Of The Wall (Yellowknife, Canada), Brad made the trek to the Wet Coast (Vancouver, Canada) to educate and equip himself with the necessary skills to become a spearhead in his trade of solving problems on the web, crafting design solutions, and speaking in code.</p>
-					
-					<p>Brad's determination and love for what he does has landed him in some pretty interesting places with some neat people. He's had the privilege of working with, and providing solutions for, numerous businesses, big &amp; small, across the Americas.</p>
-					
-					<p>Brad builds custom websites, and provides design solutions for a wide-array of clientele at his company, Brightside Studios. He regularly blogs about passive income, living your life to the fullest, and provides premium quality web design tutorials and courses for tens of thousands of amazing people desiring to master their craft.</p>
+					<?php echo $full_bio; ?>
 					
 					<hr>
 					
@@ -264,7 +302,7 @@ get_header(); ?>
 						<div class="col-xs-4">
 							<div class="num">
 								<div class="num-content">
-									41,000+ <span>students</span>
+								<?php echo $num_students ?> <span>students</span>
 								</div><!-- num-content -->
 							</div><!-- num -->
 						</div><!-- end col -->
@@ -272,7 +310,7 @@ get_header(); ?>
 						<div class="col-xs-4">
 							<div class="num">
 								<div class="num-content">
-									568 <span>reviews</span>
+								<?php echo $num_reviews ?> <span>reviews</span>
 								</div><!-- num-content -->
 							</div><!-- num -->
 						</div><!-- end col -->
@@ -280,7 +318,7 @@ get_header(); ?>
 						<div class="col-xs-4">
 							<div class="num">
 								<div class="num-content">
-									8 <span>courses</span>
+								<?php echo $num_courses ?> <span>courses</span>
 								</div><!-- num-content -->
 							</div><!-- num -->
 						</div><!-- end col -->
@@ -300,58 +338,32 @@ get_header(); ?>
 			
 				<div class="col-sm-8 col-sm-offset-2">
 					<h2>What People Are Saying About Brad</h2>
-					
-					<!-- TESTIMONIAL -->
-					<div class="row testimonial">
-						<div class="col-sm-4">
-							<img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/brennan.jpg" alt="Brennan">
-						</div><!-- end col -->
-						<div class="col-sm-8">
-							<blockquote>
-								These videos are well created, concise, fast-paced, easy to follow, and just funny enough to keep you chuckling as you're slamming out lines of code. I've taken 3 courses from this instructor. Whenever I have questions he is right there with a simple solution or a helpful suggestion to keep me going forward with the course work.
-								<cite>&mdash; Brennan, graduate of all of Brad's courses</cite>
-							</blockquote>
-						</div><!-- end col -->
-					</div><!-- row -->
-					
-					<!-- TESTIMONIAL -->
-					<div class="row testimonial">
-						<div class="col-sm-4">
-							<img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/ben.png" alt="Illustration of a man with a moustache">
-						</div><!-- end col -->
-						<div class="col-sm-8">
-							<blockquote>
-								I found Brad to be a great teacher, and a very inspiring person. It's clear he is very passionate about helping designers learn to code, and I look forward to more courses from him!
-								<cite>&mdash; Ben, graduate of Build a Website from Scratch with HTML &amp; CSS</cite>
-							</blockquote>
-						</div><!-- end col -->
-					</div><!-- row -->
-					
-					<!-- TESTIMONIAL -->
-					<div class="row testimonial">
-						<div class="col-sm-4">
-							<img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/aj.png" alt="Illustration of a man with a beard">
-						</div><!-- end col -->
-						<div class="col-sm-8">
-							<blockquote>
-								Brad is amazing and I honestly think he's the best tutor of all the courses I have taken on Udemy. Will definitely be following him in the future. Thanks Brad!
-								<cite>&mdash; AJ, graduate of Code a Responsive Website with Bootstrap 3</cite>
-							</blockquote>
-						</div><!-- end col -->
-					</div><!-- row -->
-	
-					<!-- TESTIMONIAL -->
-					<div class="row testimonial">
-						<div class="col-sm-4">
-							<img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/ernest.png" alt="Illustration of a man with a goatee">
-						</div><!-- end col -->
-						<div class="col-sm-8">
-							<blockquote>
-								Brad is an excellent instructor. His content is super high quality, and you can see the love and care put into every section. The tutorials are the perfect length, and you feel like your doing something right out the gate! I really can't believe this is free. I highly recommend taking advantage of this course.
-								<cite>&mdash; Ernest, graduate of Code Dynamic Websites with PHP</cite>
-							</blockquote>
-						</div><!-- end col -->
-					</div><!-- row -->
+
+					<?php $loop = new WP_Query( array( 
+					'post_type' => 'testimonial',
+					'orderby' => 'post_id',
+					'order' => 'ASC' 
+					) ) ?>
+
+					<?php while( $loop->have_posts() ) : $loop->the_post(); ?>
+						<!-- TESTIMONIAL -->
+						<div class="row testimonial">
+							<div class="col-sm-4">
+								<?php 
+									if ( has_post_thumbnail ) {
+										the_post_thumbnail( array( 200,200) );
+									}
+								?>
+							</div><!-- end col -->
+							<div class="col-sm-8">
+								<blockquote>
+									<?php the_content(); ?>
+									<cite>&mdash; <?php the_title(); ?></cite>
+								</blockquote>
+							</div><!-- end col -->
+						</div><!-- row -->
+
+					<?php endwhile; ?>
 					
 				</div><!-- end col -->
 				
